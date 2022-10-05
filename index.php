be76a457554bce1f63b7a06a2758529b915ec9cc
1Seva1 <sevostyanovmv@gmail.com>
2022-10-05 16:19:28 +0300

    edit structure

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
 ini_set('display_errors', 1);
-//require_once 'application/bootstrap.php';
-//$url = $_GET['url'];
-//echo $url;
-print_r ($_GET);
-var_dump ($_REQUEST);
+require_once 'application/bootstrap.php';
